337895918f4b2c5cabce3a4728ef082b5509543f
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2020-05-29 18:55:12 -0400

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -118,7 +118,6 @@ const loop = () =>{
 
 }
 
-
 setInterval(loop, 1000/60)
 
 var elems = [...Class('elem'), ...Class('card')]
@@ -211,5 +210,31 @@ document.body.addEventListener('mouseleave', e => {
 document.body.addEventListener('mouseenter', e => {
     pointer.style.opacity = 1;
 })
+
+// Lazy Loading
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to a more compatible method here
+    }
+});
 
 
